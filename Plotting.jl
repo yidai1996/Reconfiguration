5f52d449afb1c4a02b724c4314ac5fb0339f00e9
Samuel Fay <smafay6@gmail.com>
2023-01-20 11:27:27 -0500

    got number of rows and columns from excel sheet

--- Plotting.jl
+++ Plotting.jl
@@ -1,9 +1,15 @@
 using Plots, Printf, XLSX
-using Plots.PlotMeasures
+using Plots.PlotMeasures, DataFrames
 function MyPlots()
-    xf = XLSX.readxlsx("G:\\My Drive\\Research\\SVM\\Training dataset\\Training dataset.xlsx") # Feel free to try any files in your computer
-    sh1 = xf["Setpoint Tracking"]
-    sh2 = xf["Disturbance Rejection"]
+    # xf = XLSX.readxlsx("G:\\My Drive\\Research\\SVM\\Training dataset\\Training dataset.xlsx") # Feel free to try any files in your computer
+    xf = XLSX.readxlsx("C:\\Users\\sfay\\Documents\\Outputs\\Initial Condition Permutations\\top_initial_conditions.xlsx") # Feel free to try any files in your computer
+    # sh1 = xf["Setpoint Tracking"]
+    # sh2 = xf["Disturbance Rejection"]
+    sh1 = xf["Sheet1"]
+    num_rows, num_cols = size(sh1[:])
+    println(string(num_rows) * " " * string(num_cols))
+    range = "G3:G" * string(num_rows)
+    println(range)
     s = convert(Array{Float64,2},sh1["G3:G32"]) # Here is the point: instead of assigning row number (32), let the code search for the number of the last row
     percentage1 = 100*convert(Array{Float64,2},sh1["L3:L32"])
     d = convert(Array{Float64,2},sh2["G6:G42"])
@@ -16,3 +22,5 @@ function MyPlots()
     plot!([3,40],[0,0],linestyle=:dot,width=2,label=false)
     display(p2)
 end
+
+MyPlots()
