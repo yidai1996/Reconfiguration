2b2e2c190f54bcf8d679afc037cdb1cf05469fb0
Samuel Fay <smafay6@gmail.com>
2023-02-24 15:04:25 -0500

    finished permutation_values in permutate_all()

--- permutation.jl
+++ permutation.jl
@@ -267,12 +267,10 @@ function save_profile_images_initial_conditions(inputMatrix, adjacencies, distur
     end
 end
 
-# changes the configuration in the middle of running, permutates only xBs' from 0.26-0.46
+# changes the configuration in the middle of running, permutates only xBs' from 0.16-0.36
 # reactors to permutate is the list of reactor numbers to permutate xBs' on
 function permutate_setpoint(out_dir, n1, n2, Dist_T0, initial_conditions, reconfiguration_conditions,
     reactors_to_permutate)
-    # TODO rewrite the code below for permuting setpoints
-    # TODO hardcode SetChange_xB for 0.16-0.36
     N = size(n1)[1] - 1
     unique_permutations = 0
     # Nxm matrix where N is number of reactors and m is number of initial conditions
@@ -318,53 +316,6 @@ function permutate_setpoint(out_dir, n1, n2, Dist_T0, initial_conditions, reconf
 
 end
 
-# changes the configuration in the middle of running, permutates only Dist_T0
-# reactors to permutate is the list of reactor numbers to permutate xBs' on
-function permutate_temp_in(out_dir, n1, n2, Dist_T0, initial_conditions, reconfiguration_conditions,
-    reactors_to_permutate)
-    N = size(n1)[1] - 1
-    unique_permutations = 0
-    # Nxm matrix where N is number of reactors and m is number of initial conditions
-
-
-    num_permutations = 81
-    SetChange_xB = [0.15 0.15 0.15 0.15] .* reactors_to_permutate # this is hardcoded, had to change for 4R
-    step_size = repeat([1.0 1.0], N)
-    SetChange_T = [reconfiguration_conditions[i,2] - initial_conditions[i,2] for i in 1:N]
-
-    # normalizing constants make the different fields factor equally into the sums
-    n = 0
-    avg_xB = 0
-    xB_norm_const_test_1 = 11.085 / 1.971e-6
-    xB_norm_const_test_2 = 11.085 / 1.971e-6
-    avg_T = 0
-    avg_flow = 0
-    avg_heat = 0
-    avg_max_xB = 0
-    avg_max_xB_const_test_2 = 10.91 / 0.111299
-
-    # for i in ProgressBar(10:20)
-    for i in ProgressBar(0:num_permutations)
-    # for i in 0:100
-        # println(base_five)
-
-        unique_permutations += 1
-        Dist_T0 = Dist_T0 .+ step_size
-        # print("SetChange_xB: " * string(SetChange_xB))
-        image_name = (out_dir * "\\Perm_Dist_TO" * string(Dist_T0[1]) * ".png")
-        # discrepancies is an array of length 4 [qXb*dxB^2, qT*dT^2, r_flow*dFlow^2, r_heat*dHeat^2]
-        discrepancies = MPC_tracking(n1,n2,Dist_T0,SetChange_xB,SetChange_T,
-        1,1e7,1e7,1e-3,1e9,90,1000,[8 15],15,initial_conditions;tmax=5000, print=false,
-        save_plots=true, plot_name=image_name)
-        n += 1
-
-
-    end
-
-    # TODO have MPC_tracking always write full data to file, in here copy that file
-    # and rename to current input values
-
-end
 
 function save_profile_images_permutation_setpoints(inputMatrix, n1,n2, reactors_to_permutate, disturbances, out_dir)
     # TODO rewrite the code below for permuting setpoint
@@ -378,6 +329,131 @@ function save_profile_images_permutation_setpoints(inputMatrix, n1,n2, reactors_
         MPC_tracking(n1,n2,disturbances,1,1e7,1e7,1e-3,1e9,90,1000,[8 15],initial_values;tmax=5000, print=false, save_plots=true, plot_name=image_name)
         count += 1
     end
+end
+
+# permutates initial conditions, setpoint, and temp in
+# full documentation at https://docs.google.com/document/d/1M9YBCu95bkd8m2DBJw7dbVq65WrtEwY9orqvBqOqalA/edit#
+# initial_conditions is an Nx4 matrix where N is the number of reactors and 4 is [T0, Ts, xBs, setpoint_change].
+# ranges_steps is an 4x3 matrix where each row is [start, end, step_size].
+function permutate_all(out_dir, n1, n2, initial_conditions, ranges_steps,
+    reactors_to_permutate)
+    N = size(n1)[1] - 1
+    num_variables = size(initial_conditions)[2]
+    visited_dictionary = Dict() # [T0, Ts, xBs, setpoint_change] => {0,1} - don't run the same conditions twice
+    unique_permutations = 0
+    steps = floor.((ranges_steps[:,2] .- ranges_steps[:,1]) ./ ranges_steps[:,3])
+    max_steps = maximum(steps)
+    permutation_values = zeros(Float64, (max_steps, num_variables, N))
+    for n in 1:N # for each reactor
+        for i in 1:max_steps # for every row 
+            for j in 1:num_variables # for each number in [T0, Ts, xBs, setpoint_change]
+                if i <= steps[j]
+                    permutation_values[i,j,n] = initial_conditions[n,j] + i * ranges_steps[j,3]
+                else
+                    permutation_values[i,j,n] = initial_conditions[n,j] # set to original weight if already past max value
+            end
+        end
+    end
+    display(permutation_values)
+# add
+
+    num_permutations = max_steps^num_variables - 1 # iterate in base max_steps through all possible permutations
+
+    # for i in ProgressBar(10:20)
+    for i in ProgressBar(0:num_permutations)
+    # for i in 0:100
+        base_max_steps = string(i, base=max_steps, pad=size(original_values)[2])
+        # println(base_max_steps)
+        #TODO handle case where base_max_steps has hex values in it
+        current_values = deepcopy(original_values)
+        for n in 1:N
+            for j in 1:4
+                char = base_max_steps[j]
+                current_values[n,j] = permutation_values[parse(Int64, char) + 1, j,n]
+            end
+        end
+        println("current_values=",current_values)
+        current_displacements = current_values[1,:] .- original_values[1,:]
+
+        # check to see if permutation already in top permutations
+        already_exists = false
+        for j in 1:num_permutations
+            if completed_permutations[j,:] == current_displacements
+                already_exists = true
+            end
+        end
+        completed_permutations[i+1,:] = current_displacements
+        if already_exists
+            continue
+        end
+        unique_permutations += 1
+
+        # discrepancies is an array of length 4 [qXb*dxB^2, qT*dT^2, r_flow*dFlow^2, r_heat*dHeat^2]
+        discrepancies = MPC_tracking(adjacencies, adjacencies,disturbances,[0 0;0 0;0 0],[0 0;0 0;0 0],1,1e7,1e7,1e-3,1e9,90,1000,[8 15],15,current_values
+        ;tmax=5000, print=false)
+        n += 1
+        avg_xB += discrepancies[1]
+        avg_T += discrepancies[2]
+        avg_flow += discrepancies[3]
+        avg_heat += discrepancies[4]
+        avg_max_xB += discrepancies[5]
+
+        # println("Discrepancies: $discrepancies")
+        sum_discrepancies = sum(discrepancies) # basic sum
+        # sum_discrepancies = xB_norm_const_test_1 * discrepancies[1] + discrepancies[2] # test 1
+        # sum_discrepancies = xB_norm_const_test_2 * discrepancies[1] + discrepancies[2] + avg_max_xB_const_test_2 * discrepancies[5] # test 2
+        # sum_discrepancies = discrepancies[5] # test 3
+        # sum_discrepancies = xB_norm_const_test_1 * discrepancies[1] + discrepancies[2] + discrepancies[6] # test 4
+        # sum_discrepancies = discrepancies[1] # just xB
+
+
+        m = minimum(top[:,10]) # find the worst performing permutations
+        if sum_discrepancies > m
+            insert_index = findall(x -> x==m, top[:,10])[1]
+            top[insert_index,1:3] .= current_values[1,:] .- original_values[1,:]
+            top[insert_index,4:9] = discrepancies
+            top[insert_index,10] = sum_discrepancies
+        end
+        # for j in 1:10
+        #     # println("$(top_ten[j,10]) $sum_discrepancies")
+        #     is_full = maximum(top_ten[:,10]) != Inf
+        #     # println("$(top_ten[:,10]) $is_full")
+        #     if top_ten[j,10] > sum_discrepancies
+        #         !is_full && top_ten[j,10] != Inf && continue
+        #         top_ten[insert_index,1:5] = current_weights
+        #         top_ten[insert_index,6:9] = discrepancies
+        #         top_ten[insert_index,10] = sum_discrepancies
+        #         break
+        #     end
+        # end
+    end
+    println("$(unique_permutations) unique permutations found!")
+    for i in 1:num_permutations
+        println(completed_permutations[i,:])
+    end
+    println("Average discrepancies")
+    print("xB: $(avg_xB/n)\n")
+    print("T: $(avg_T/n)\n")
+    print("Heat: $(avg_heat/n)\n")
+    print("Flow: $(avg_flow/n)\n")
+    print("max xB: $(avg_max_xB/n)\n")
+
+    top = round.(top,digits=9)
+    display(top)
+    println("writing top configurations to file")
+    top_file = out_dir * "\\top_initial_conditions.txt"
+    top_excel_file = out_dir * "\\top_initial_conditions.xlsx"
+    touch(top_file)
+    file = open(top_file, "w")
+    column_names = ["T0", "Ts", "xBs", "xBtvt", "Tvt", "flowvt", "heatvt", "max_Tvt", "tt_stable", "PI"]
+    # write to text file
+    write(file, join(column_names, "\t") * "\n")
+    writedlm(file, top)
+    # write to excel file
+    XLSX.writetable(top_excel_file, [top[:, i] for i in 1:size(top,2)], column_names, overwrite=true)
+    close(file)
+
+    return top
 end
 
 # format: 1-3,2-3 (mixing configuration)
